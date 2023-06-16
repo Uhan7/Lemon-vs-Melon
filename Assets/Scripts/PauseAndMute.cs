02110bb23959553feaaa435875a415b2066233c6
Uhan7 <luckyfellowfella@gmail.com>
2023-06-16 23:29:01 +0800

    Add temp pause n mute

--- Assets/Scripts/PauseAndMute.cs
+++ Assets/Scripts/PauseAndMute.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PauseAndMute: MonoBehaviour
 {
 
+    public GameObject PauseMenu;
+
     private bool paused;
+    private bool muted;
 
     void Start()
     {
@@ -18,11 +21,29 @@ public class PauseAndMute: MonoBehaviour
         {
             paused = true;
             Time.timeScale = 0;
+            PauseMenu.SetActive(true);
+            AudioListener.volume = 0.4f;
         }
         else
         {
             paused = false;
             Time.timeScale = 1;
+            PauseMenu.SetActive(false);
+            AudioListener.volume = 1;
+        }
+    }
+
+    public void Mute()
+    {
+        if (!muted)
+        {
+            muted = true;
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            muted = false;
+            AudioListener.volume = 1;
         }
     }
 
